f5b5c75fe0df2f00b5e397498d1fc15e1b0a479d
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-29 16:48:40 -0300

    5 requisito

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,58 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Pokedex.js tests', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+
+    const pokedexTextH2 = screen.getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+
+    expect(pokedexTextH2).toBeInTheDocument();
+  });
+
+  test('', () => {
+    renderWithRouter(<App />);
+    const buttonNextPokemon = screen.getByTestId('next-pokemon');
+
+    userEvent.click(buttonNextPokemon);
+    const charmander = screen.getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+
+    userEvent.click(buttonNextPokemon);
+    const caterpie = screen.getByText('Caterpie');
+    expect(caterpie).toBeInTheDocument();
+
+    userEvent.click(buttonNextPokemon);
+    userEvent.click(buttonNextPokemon);
+    userEvent.click(buttonNextPokemon);
+    userEvent.click(buttonNextPokemon);
+    userEvent.click(buttonNextPokemon);
+    userEvent.click(buttonNextPokemon);
+    userEvent.click(buttonNextPokemon);
+
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+
+    const buttonPoison = screen.getByRole('button', { name: 'Poison' });
+    expect(buttonPoison).toBeInTheDocument();
+    userEvent.click(buttonPoison);
+
+    const ekans = screen.getByText('Ekans');
+    expect(ekans).toBeInTheDocument();
+
+    const allButtunsType = screen.getAllByTestId('pokemon-type-button');
+    const SEVEN = 7;
+    expect(allButtunsType.length).toBe(SEVEN);
+
+    const buttonAll = screen.getByRole('button', { name: 'All' });
+    userEvent.click(buttonAll);
+
+    expect(pikachu).toBeInTheDocument();
+  });
+});

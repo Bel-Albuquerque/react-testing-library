746ccff35cfb83c3acb6a60c983acf2c775c50cd
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-29 19:26:11 -0300

    ultimo requisito

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,61 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+import pokemons from '../data';
+
+const caminhoPikachu = 'pokemons/25';
+
+describe('Pokemon.js tests', () => {
+  test(`Teste se as informações detalhadas do Pokémon selecionado
+  são mostradas na tela.`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(caminhoPikachu);
+
+    const pikachu = pokemons[0];
+
+    const titleDetailsPage = screen.getByRole('heading', {
+      level: 2,
+      name: `${pikachu.name} Details`,
+    });
+    expect(titleDetailsPage).toBeInTheDocument();
+
+    const summary = screen.getByRole('heading', {
+      level: 2,
+      name: 'Summary',
+    });
+    expect(summary).toBeInTheDocument();
+  });
+
+  test(`Teste se existe na página uma seção com os mapas contendo
+  as localizações do pokémon`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(caminhoPikachu);
+
+    const pikachu = pokemons[0];
+
+    const titleMapLocations = screen.getByRole('heading', {
+      level: 2,
+      name: `Game Locations of ${pikachu.name}`,
+    });
+    expect(titleMapLocations).toBeInTheDocument();
+
+    const mapImages = screen.getAllByAltText('Pikachu location');
+    expect(mapImages.length).toBe(pikachu.foundAt.length);
+    expect(mapImages[0]).toHaveAttribute('src', pikachu.foundAt[0].map);
+    expect(mapImages[1]).toHaveAttribute('src', pikachu.foundAt[1].map);
+  });
+
+  test(`Teste se existe na página uma seção com os mapas contendo
+  as localizações do pokémon`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(caminhoPikachu);
+
+    const pokemonFvoritado = screen.getByLabelText('Pokémon favoritado?');
+    expect(pokemonFvoritado).toBeInTheDocument();
+    userEvent.click(pokemonFvoritado);
+    const checkbox = screen.getByRole('checkbox', { checked: true });
+    expect(checkbox).toBeInTheDocument();
+  });
+});
